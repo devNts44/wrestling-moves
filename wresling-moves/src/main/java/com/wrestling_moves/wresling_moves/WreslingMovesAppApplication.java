485c0d3fa8935e0ba3ccc69231b9a1fb787d8edc
Islam Massat <yusikboxe44@gmail.com>
2024-12-21 12:21:11 +0100

    Initialisaion du projet Spring Boot

---
+++ wresling-moves/src/main/java/com/wrestling_moves/wresling_moves/WreslingMovesAppApplication.java
@@ -0,0 +1,13 @@
+package com.wrestling_moves.wresling_moves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WreslingMovesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WreslingMovesAppApplication.class, args);
+	}
+
+}
---
+++ wresling-moves/src/test/java/com/wrestling_moves/wresling_moves/WreslingMovesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrestling_moves.wresling_moves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WreslingMovesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
