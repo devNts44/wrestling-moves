fcab194928648252f7f93d80f9a526b3d070fa6c
Islam Massat <yusikboxe44@gmail.com>
2024-12-25 15:30:53 +0100

    ajoute un test pour tester que le controlleur fonctionne correctement

---
+++ wrestling-moves/src/main/java/com/wrestling_moves/entity/User.java
@@ -0,0 +1,43 @@
+package com.wrestling_moves;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.Id;
+import org.springframework.stereotype.Component;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
