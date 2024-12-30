a6a55ded1e6bbcb1c67d5beeb6e4f79c8cd69822
Islam Massat <yusikboxe44@gmail.com>
2024-12-30 19:16:59 +0100

    teste que le controlleur wrestler est correctement géré & que la requête GET vers la racine est Ok

--- wrestling-moves/src/test/java/com/wrestling_moves/WrestlingVideoAppApplicationTests.java
+++ wrestling-moves/src/test/java/com/wrestling_moves/WrestlingVideoAppApplicationTests.java
@@ -1,6 +1,6 @@
 package com.wrestling_moves;
 
-import com.wrestling_moves.controller.HomeController;
+import com.wrestling_moves.controller.WrestlerController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,30 +8,32 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.ui.Model;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest()
 @AutoConfigureMockMvc
 class WrestlingVideoAppApplicationTests {
+
+	@Autowired
+	private WrestlerController wrestlerController;
 
 	@Autowired
 	private MockMvc mvc;
 
 	@Test
-	public void contextLoads(){
-
+	void shouldLoadWrestlerControllerIntoContext() throws Exception {
+		assertThat(wrestlerController).isNotNull();
 	}
 	@Test
-	public void getMappingIsOk() throws Exception {
+	public void shouldReturnOkForRootEndpoint() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk());
 	}
-	@Test
-	public void getMappingUserIsOk() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/users").accept(MediaType.APPLICATION_JSON))
+	/*@Test
+	public void getMappingWrestlersIsOk() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/wrestlers").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk());
-	}
+	}*/
 }
