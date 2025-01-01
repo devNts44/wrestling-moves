ce40e617f7177bd990c54471e61d4c3d89378565
Islam Massat <yusikboxe44@gmail.com>
2025-01-01 14:19:56 +0100

    met en place une table pour Wrestler et insère un tuple

---
+++ wrestling-moves/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO WRESTLER (username, password, email, first_name, last_name)
+VALUES ('yus_ka84', 'securepassword', 'yuska@example.com', 'Islam', 'Massat');
---
+++ wrestling-moves/src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE WRESTLER (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255),
+    password VARCHAR(255),
+    email VARCHAR(255),
+    first_name VARCHAR(255),
+    last_name VARCHAR(255)
+);
