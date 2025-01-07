d269afae32fa2b6307d4acd22b5f409a1b9da2b2
Islam Massat <yusikboxe44@gmail.com>
2025-01-08 00:07:56 +0100

    ajoute les fichiers properties

--- wrestling-moves/src/main/resources/data.sql
+++ wrestling-moves/src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO WRESTLER (username, password, email, first_name, last_name)
-VALUES ('yus_ka84', 'securepassword', 'yuska@example.com', 'Islam', 'Massat');
+VALUES ('yus_ka84', 'Passw0rd!', 'yuska@example.com', 'Islam', 'Massat');
